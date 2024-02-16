c255e9d2f987da907b421a07d6ffcf0594475f97
jay mbans <jordan@mbanefo.com>
2024-02-15 19:31:57 -0500

    created baseline maze class

---
+++ Window.py
@@ -0,0 +1,34 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+
+  def __init__(self, width, height):
+    # static member variables
+    self.width = width
+    self.height = height
+
+    # Tkinter class variables
+    self.root = Tk()
+    self.root.title("Maze Game")
+    self.canvas = Canvas(self.root, width=self.width, height=self.height)
+    self.is_running = False
+
+    # Start Tkinter window
+    self.canvas.pack()
+    self.root.protocol("WM_DELETE_WINDOW", self.close)
+    
+  def close(self):
+    self.is_running = False
+
+  def redraw(self):
+    self.root.update()
+    self.root.update_idletasks()
+
+  def wait_for_close(self):
+    self.is_running = True
+    while(self.is_running):
+      self.redraw()
+
+
+win = Window(800,600)
+win.wait_for_close()
